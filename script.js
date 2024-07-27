d290fa85d36bbeff7cff5c80440503f86ffb08e9
azhakarasan3549 <alagualagu189@gmail.com>
2024-07-27 16:06:32 +0530

    for 4

---
+++ script.js
@@ -0,0 +1,15 @@
+
+barcon.addEventListener('click',()=>{
+    const bar= document.getElementById('bar')
+    const barcon= document.getElementById('barcon')
+   console.log("click")
+    if (bar.style.display=='block') {
+        bar.style.display='none';
+         barcon.style.color="black"
+    }
+    else{
+        bar.style.display='block'
+         barcon.style.color="white"
+    }
+})
+  
