ebaf09ea319387e07aa022bb3b729cc99bb7a90f
azhakarasan3549 <alagualagu189@gmail.com>
2024-08-01 11:10:02 +0530

    for 7

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 barcon.addEventListener('click',()=>{
     const bar= document.getElementById('bar')
     const barcon= document.getElementById('barcon')
+    
    console.log("click")
     if (bar.style.display=='block') {
         bar.style.display='none';
@@ -9,9 +10,9 @@ barcon.addEventListener('click',()=>{
     }
     else{
         bar.style.display='block'
-        //  bar.style.transitionDuration='10s'
-        // bar.style.translate='0px'
          barcon.style.color="white"
     }
+    
 })
+
   
