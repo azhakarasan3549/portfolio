19f9bd12cf240a1a41f30c35846ec4d8314236ef
azhakarasan3549 <alagualagu189@gmail.com>
2024-09-04 21:09:42 +0530

    for 11.5

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+
 barcon.addEventListener('click',()=>{
     var bar= document.getElementById('bar')
     const barcon= document.getElementById('barcon')
    console.log("click")
     if (bar.style.display=='block') {
         bar.style.display='none';
-         barcon.style.color="white"
+         barcon.style.color="white";
     }
     else{
-        bar.style.display='block'
-         barcon.style.color="white"
+        bar.style.display='block';
+         barcon.style.color="white";
     }
 })
+
 background.addEventListener('click',()=>{
     const background=document.getElementById('background')
         bar.style.display='none';
